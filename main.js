73b539faabb3178e78d30662171fe23d8b1b9c17
Bruno Carrara <brunocarrarabpc@gmail.com>
2021-09-02 03:27:59 -0300

    checks if you are in a voice channel and responds to it

--- main.js
+++ main.js
@@ -1,19 +1,115 @@
-const { Intents } = require("discord.js");
+const { Intents, ReactionUserManager, Channel, VoiceChannel } = require("discord.js");
+const ytdl = require("ytdl-core");
 
 require("dotenv").config();
 
 const {Client} = require('discord.js');
-const client = new Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
+const client = new Client({intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES", "GUILD_PRESENCES"]});
+const prefix = "-";
+
+const servidores = {
+    'server':{
+        connection: null,
+        dispatcher: null
+    }
+}
+var queue = [50];
+var arg = [0];
+
 
 client.on('ready', () => {
     console.log(`${client.user.username} is now online ;)`);
 });
 
 //prefix verify
-client.on('messageCreate', (message) => {
+client.on('messageCreate', async (message) => {
     if (message.author.bot) return;
-    if (message.content.startsWith('-')); //ident(message);
+    if (message.content.startsWith(prefix)){
+        const [cmd, ...args] = message.content.trim().substring(prefix.length).split(" ");
+        console.log(cmd);
+        console.log(args);
+        if(args === " ") args = null;
+        if(cmd === "p"){ 
+            if(!message.member.voice.channel){  
+                message.channel.send('por favor entre em um canal ne camarada');
+                return;
+                }
+            
+            //exports.run(args);
+            let url = args;
+            if(!url) return message.channel.send("eitaaa, nao é isso nao");
+
+            let verify = ytdl.validateURL(url);
+            if(!verify) return message.channel.send("Precisa ser link do youtube amigao!");
+            message.channel.send('A musica selecionada tocara em breve');
+            let connection = await message.member.voice.channel.join(VoiceChannel);
+            
+}
+            
+            
+            
+            
+            //servidores.server.connection = await message.member.voice.channel.join();
+
+            //let songData = ytdl.getInfo(url);
+            /*let song = {
+                title: songData.videoDetails.title,
+                url: songData.videoDetails.video_url
+            };*/
+           
+            
+            let dispatcher = connection.play(ytdl(url), {filter: 'audioonly', quality:'highest'});
+
+            //message.channel.send(`[${song.title}][${song.url}] começou a tocar, seguuura`);
+            message.channel.send(' começou a tocar, seguuura');
+            
+
+            }
+           
+
+            
+             
+         //ident(message);
+        
+
+        
+    
+    
+
 });
+
+
+
+   
+
+    
+
+/*exports.run = async(message, args) =>{
+    let url = args;
+    if(!url) return message.channel.send("Só trabalho com links no momento!");
+
+    let verify = await ytdl.validateURL(url);
+    if(!verify) return message("Precisa ser do youtube amigao!");
+
+    let songData = await ydtl.getInfo(url);
+    let song = {
+        title: songData.videoDetails.title,
+        url: songData.videoDetails.video_url
+    };
+
+    let connection = await message.member.voice.channel.join();
+    let dispatcher = connection.play(ytdl(url), {filter: 'audioonly', quality:'highest'});
+
+    message.channel.send(`[${song.title}][${song.url}] começou a tocar, seguuura`)
+
+}
+
+exports.help = {
+    name:'play',
+    aliases: ['tocar']
+}
+*/
+
 
 //dialog
 client.on('messageCreate', (message) => {
